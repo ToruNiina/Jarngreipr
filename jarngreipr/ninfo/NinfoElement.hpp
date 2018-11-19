a9ed69e668f63ebb54f2893526205b751ed48749
ToruNiina <niina.toru.68u@gmail.com>
2018-11-19 21:17:35 +0900

    simplify NinfoElement implementation
    remove complicated `char_seq`, put enum and helper meta-functions instead

--- jarngreipr/ninfo/NinfoElement.hpp
+++ jarngreipr/ninfo/NinfoElement.hpp
@@ -10,50 +10,60 @@
 namespace jarngreipr
 {
 
-namespace detail
+enum class NinfoKind
 {
-
-template<char ... args>
-struct char_sequence
-{
-    using value_type = char;
-    static constexpr std::size_t size() noexcept {return sizeof ... (args);}
+    bond,
+    angl,
+    dihd,
+    aicg13,
+    aicg14,
+    aicgdih,
+    contact,
+    basepair,
+    basestack,
+    pdpwm,
 };
 
-template<typename T> struct char_sequence_string_generator;
-
-template<char ... args>
-struct char_sequence_string_generator<char_sequence<args...>>
-{
-    static const char* generate() noexcept
-    {
-        constexpr std::size_t len = char_sequence<args...>::size()+1;
-        static constexpr char retval[len] = {args..., '\0'};
-        return retval;
-    }
-};
-
-// n3559
-using ninfo_bond_prefix      = char_sequence<'b','o','n','d'>;
-using ninfo_angl_prefix      = char_sequence<'a','n','g','l'>;
-using ninfo_dihd_prefix      = char_sequence<'d','i','h','d'>;
-using ninfo_aicg13_prefix    = char_sequence<'a','i','c','g','1','3'>;
-using ninfo_aicg14_prefix    = char_sequence<'a','i','c','g','1','4'>;
-using ninfo_aicgdih_prefix   = char_sequence<'a','i','c','g','d','i','h'>;
-using ninfo_contact_prefix   = char_sequence<'c','o','n','t','a','c','t'>;
-using ninfo_basepair_prefix  = char_sequence<'b','a','s','e','p','a','i','r'>;
-using ninfo_basestack_prefix = char_sequence<'b','a','s','e','s','t','a','c','k'>;
-using ninfo_pdpwm_prefix     = char_sequence<'p','d','p','w','m'>;
-
-} // detail
+template<NinfoKind kind>
+struct ninfo_prefix;
+template<>
+struct ninfo_prefix<NinfoKind::bond>
+{static constexpr auto prefix = "bond";};
+template<>
+struct ninfo_prefix<NinfoKind::angl>
+{static constexpr auto prefix = "angl";};
+template<>
+struct ninfo_prefix<NinfoKind::dihd>
+{static constexpr auto prefix = "dihd";};
+template<>
+struct ninfo_prefix<NinfoKind::aicg13>
+{static constexpr auto prefix = "aicg13";};
+template<>
+struct ninfo_prefix<NinfoKind::aicg14>
+{static constexpr auto prefix = "aicg14";};
+template<>
+struct ninfo_prefix<NinfoKind::aicgdih>
+{static constexpr auto prefix = "aicgdih";};
+template<>
+struct ninfo_prefix<NinfoKind::contact>
+{static constexpr auto prefix = "contact";};
+template<>
+struct ninfo_prefix<NinfoKind::basepair>
+{static constexpr auto prefix = "basepair";};
+template<>
+struct ninfo_prefix<NinfoKind::basestack>
+{static constexpr auto prefix = "basestack";};
+template<>
+struct ninfo_prefix<NinfoKind::pdpwm>
+{static constexpr auto prefix = "pdpwm";};
 
 template<typename realT, std::size_t N_units, std::size_t N_particles,
-         std::size_t N_coefs, typename prefixT>
+         std::size_t N_coefs, NinfoKind Kind>
 struct NinfoElement
 {
-    using real_type   = realT;
-    using prefix_type = prefixT;
-    static char const* prefix;
+    using real_type = realT;
+    static constexpr auto        kind   = Kind;
+    static constexpr const char* prefix = ninfo_prefix<Kind>::prefix;
 
     std::size_t                          id;
     std::array<std::size_t, N_units>     units;
@@ -64,34 +74,56 @@ struct NinfoElement
 };
 
 template<typename realT, std::size_t N_u, std::size_t N_p, std::size_t N_c,
-         typename prefixT>
-char const* NinfoElement<realT, N_u, N_p, N_c, prefixT>::prefix =
-    detail::char_sequence_string_generator<prefixT>::generate();
+         NinfoKind kind>
+constexpr const char* NinfoElement<realT, N_u, N_p, N_c, kind>::prefix;
 
 template<typename realT>
-using NinfoBond      = NinfoElement<realT, 2, 2,  4, detail::ninfo_bond_prefix>;
+using NinfoBond      = NinfoElement<realT, 2, 2,  4, NinfoKind::bond>;
 template<typename realT>
-using NinfoAngl      = NinfoElement<realT, 2, 3,  4, detail::ninfo_angl_prefix>;
+using NinfoAngl      = NinfoElement<realT, 2, 3,  4, NinfoKind::angl>;
 template<typename realT>
-using NinfoDihd      = NinfoElement<realT, 2, 4,  5, detail::ninfo_dihd_prefix>;
+using NinfoDihd      = NinfoElement<realT, 2, 4,  5, NinfoKind::dihd>;
 template<typename realT>
-using NinfoAicg13    = NinfoElement<realT, 2, 3,  5, detail::ninfo_aicg13_prefix>;
+using NinfoAicg13    = NinfoElement<realT, 2, 3,  5, NinfoKind::aicg13>;
 template<typename realT>
-using NinfoAicg14    = NinfoElement<realT, 2, 4,  5, detail::ninfo_aicg14_prefix>;
+using NinfoAicg14    = NinfoElement<realT, 2, 4,  5, NinfoKind::aicg14>;
 template<typename realT>
-using NinfoAicgdih   = NinfoElement<realT, 2, 4,  5, detail::ninfo_aicgdih_prefix>;
+using NinfoAicgdih   = NinfoElement<realT, 2, 4,  5, NinfoKind::aicgdih>;
 template<typename realT>
-using NinfoContact   = NinfoElement<realT, 2, 2,  4, detail::ninfo_contact_prefix>;
+using NinfoContact   = NinfoElement<realT, 2, 2,  4, NinfoKind::contact>;
 template<typename realT>
-using NinfoBasePair  = NinfoElement<realT, 2, 2,  4, detail::ninfo_basepair_prefix>;
+using NinfoBasePair  = NinfoElement<realT, 2, 2,  4, NinfoKind::basepair>;
 template<typename realT>
-using NinfoBaseStack = NinfoElement<realT, 2, 2,  4, detail::ninfo_basestack_prefix>;
+using NinfoBaseStack = NinfoElement<realT, 2, 2,  4, NinfoKind::basestack>;
 template<typename realT>
-using NinfoPDPWM     = NinfoElement<realT, 1, 1, 10, detail::ninfo_pdpwm_prefix>;
+using NinfoPDPWM     = NinfoElement<realT, 1, 1, 10, NinfoKind::pdpwm>;
 
 // XXX: BE CAREFUL. In the format of PDPWM, the second column group (normally imp)
 //      has nil value, filled with 999 and the third column group (normally impun)
 //      represents imp.
+
+template<NinfoKind kind, typename realT>
+struct ninfo_type_of;
+template<typename realT>
+struct ninfo_type_of<NinfoKind::bond,      realT> {using type = NinfoBond<realT>;};
+template<typename realT>
+struct ninfo_type_of<NinfoKind::angl,      realT> {using type = NinfoAngl<realT>;};
+template<typename realT>
+struct ninfo_type_of<NinfoKind::dihd,      realT> {using type = NinfoDihd<realT>;};
+template<typename realT>
+struct ninfo_type_of<NinfoKind::aicg13,    realT> {using type = NinfoAicg13<realT>;};
+template<typename realT>
+struct ninfo_type_of<NinfoKind::aicg14,    realT> {using type = NinfoAicg14<realT>;};
+template<typename realT>
+struct ninfo_type_of<NinfoKind::aicgdih,   realT> {using type = NinfoAicgdih<realT>;};
+template<typename realT>
+struct ninfo_type_of<NinfoKind::contact,   realT> {using type = NinfoContact<realT>;};
+template<typename realT>
+struct ninfo_type_of<NinfoKind::basepair,  realT> {using type = NinfoBasePair<realT>;};
+template<typename realT>
+struct ninfo_type_of<NinfoKind::basestack, realT> {using type = NinfoBaseStack<realT>;};
+template<typename realT>
+struct ninfo_type_of<NinfoKind::pdpwm,     realT> {using type = NinfoPDPWM<realT>;};
 
 } // jarngreipr
 #endif// JARNGREIPR_NINFO_ELEMENT_HPP
