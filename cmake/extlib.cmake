f3ecf174a6c0f0cfe8c5644ea8bd942492ff47a2
ToruNiina <niina.toru.68u@gmail.com>
2019-05-27 23:36:07 +0900

    feat: simplify cmake script by builtin functions
    use `file(DOWNLOAD <url> <file> [options...])`

--- cmake/extlib.cmake
+++ cmake/extlib.cmake
@@ -2,37 +2,31 @@ if(BOOST_ROOT)
     find_package(Boost 1.67.0 REQUIRED)
     include_directories(${Boost_INCLUDE_DIRS})
 else()
-    if(EXISTS "${JARNGREIPR_EXTLIB_DIR}/boost_1_67_0/boost/version.hpp")
+    if(EXISTS "${PROJECT_SOURCE_DIR}/extlib/boost_1_67_0/boost/version.hpp")
+
         message(STATUS "boost 1.67.0 exists.")
-        include_directories("${JARNGREIPR_EXTLIB_DIR}/boost_1_67_0")
+        include_directories("${PROJECT_SOURCE_DIR}/extlib/boost_1_67_0")
+
     else()
-        find_program(WGET wget)
-        if(NOT WGET)
-            message(FATAL_ERROR "wget is not installed. Can't download BOOST library.")
-        endif()
-        message(STATUS "downloading Boost 1.67.0 to ./extlib/ ...")
-        execute_process(COMMAND "${WGET}" https://dl.bintray.com/boostorg/release/1.67.0/source/boost_1_67_0.tar.bz2
-            WORKING_DIRECTORY "${JARNGREIPR_EXTLIB_DIR}"
-            RESULT_VARIABLE DOWNLOAD_BOOST_RESULT OUTPUT_QUIET ERROR_QUIET)
-        if(NOT ${DOWNLOAD_BOOST_RESULT} EQUAL "0")
-            message(FATAL_ERROR "failed to download Boost 1.67.0. please confirm network connections.")
+        message(STATUS "Boost library not found. Donwloading...")
+
+        file(DOWNLOAD https://dl.bintray.com/boostorg/release/1.67.0/source/boost_1_67_0.tar.bz2
+            ${PROJECT_SOURCE_DIR}/extlib/boost_1_67_0.tar.bz2
+            EXPECTED_HASH SHA256=2684c972994ee57fc5632e03bf044746f6eb45d4920c343937a465fd67a5adba
+            STATUS JARNGREIPR_DOWNLOAD_BOOST_STATUS)
+
+        # check status
+        # STATUS variable is a list.
+        # If the first element is not 0, it means downloading is failed.
+        # And the second element has the error reason as a string (if any).
+        list(GET JARNGREIPR_DOWNLOAD_BOOST_STATUS 0 JARNGREIPR_DOWNLOAD_BOOST_RESULT)
+        if(NOT ${JARNGREIPR_DOWNLOAD_BOOST_RESULT} EQUAL 0)
+            list(GET JARNGREIPR_DOWNLOAD_BOOST_STATUS 1 JARNGREIPR_DOWNLOAD_BOOST_ERROR)
+            message(FATAL_ERROR "failed to download Boost 1.67.0."
+                ${JARNGREIPR_DOWNLOAD_BOOST_ERROR})
         endif()
 
-        execute_process(COMMAND which sha256sum RESULT_VARIABLE SHA256SUM_EXISTS OUTPUT_QUIET ERROR_QUIET)
-        execute_process(COMMAND which shasum    RESULT_VARIABLE SHASUM_EXISTS    OUTPUT_QUIET ERROR_QUIET)
-        if(${SHA256SUM_EXISTS} EQUAL "0")
-            execute_process(COMMAND sha256sum --check boost_1_67_0_tar_bz2_sha256sum.dat
-                WORKING_DIRECTORY "${JARNGREIPR_EXTLIB_DIR}"
-                RESULT_VARIABLE   DOWNLOADED_SHA256SUM_RESULT)
-        elseif(${SHASUM_EXISTS} EQUAL "0")
-            execute_process(COMMAND shasum --algorithm 256 --check boost_1_67_0_tar_bz2_sha256sum.dat
-                WORKING_DIRECTORY "${JARNGREIPR_EXTLIB_DIR}"
-                RESULT_VARIABLE   DOWNLOADED_SHA256SUM_RESULT)
-        endif()
-
-        if(NOT ${DOWNLOADED_SHA256SUM_RESULT} EQUAL "0")
-            message(FATAL_ERROR "boost_1_67_0.tar.bz2 has invalid sha256sum!")
-        endif()
+        message(STATUS "downloading completed. Unpacking...")
 
         execute_process(COMMAND tar xf boost_1_67_0.tar.bz2
             WORKING_DIRECTORY "${JARNGREIPR_EXTLIB_DIR}"
