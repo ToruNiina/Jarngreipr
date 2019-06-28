1a6746a33a18bf886c7707133c18bf52f2843f41
ToruNiina <niina.toru.68u@gmail.com>
2019-06-28 19:33:15 +0900

    :alien: update table writers

--- jarngreipr/io/write_forcefield.hpp
+++ jarngreipr/io/write_forcefield.hpp
@@ -1,211 +1,274 @@
 #ifndef JARNGREIPR_WRITE_FORCEFIELD_HPP
 #define JARNGREIPR_WRITE_FORCEFIELD_HPP
 #include <extlib/toml/toml.hpp>
-#include <mjolnir/util/get_toml_value.hpp>
-#include <jarngreipr/io/write_toml_value.hpp>
+#include <jarngreipr/io/write_error.hpp>
+#include <algorithm>
+#include <ostream>
 
 namespace jarngreipr
 {
 
-template<typename charT, typename traits>
-void write_local_forcefield(
-        std::basic_ostream<charT, traits>& os, const toml::Table& ff)
+//
+// Without considering the readability, this function is not needed because
+// toml11 has a serializer. But to prettify the output, some sorting and
+// extra formatting stuff is needed.
+//
+template<typename charT, typename traits, typename Comment,
+         template<typename...> class Map, template<typename...> class Array>
+std::basic_ostream<charT, traits>&
+write_local_forcefield(std::basic_ostream<charT, traits>& os,
+                       const toml::basic_value<Comment, Map, Array>& ff)
 {
-    os << "[[forcefields.local]]\n";
-    os << "interaction = ";
-    write_toml_value(os, mjolnir::toml_value_at(
-                ff, "interaction", "write_local_forcefield"));
-    os << '\n';
-
-    os << "potential = ";
-    write_toml_value(os, mjolnir::toml_value_at(
-                ff, "potential", "write_local_forcefield"));
-    os << '\n';
-
-    os << "topology = ";
-    write_toml_value(os, mjolnir::toml_value_at(
-                ff, "topology", "write_local_forcefield"));
-    os << '\n';
-
-    const auto& params = mjolnir::toml_value_at(
-        ff, "parameters", "write_local_forcefield").cast<toml::value_t::Array>();
-    if(params.empty()) {return;}
-
-    /* ============ prepair some values for formating parameters ============ */
-    std::size_t max_index = 0;
-    for(const auto& p : params)
+    if(!ff.comment().empty())
     {
-        const auto& ptable = p.cast<toml::value_t::Table>();
-        if(ptable.count("indices"))
+        // TODO later (toml11-v3.0.0), this can be refactored into
+        //      `os << local.comment();`
+        for(const auto& c : ff.comment())
         {
-            const auto idxs = toml::get<std::vector<std::size_t>>(
-                mjolnir::toml_value_at(ptable, "indices",
-                    "jarngreipr: generated local forcefield"));
-
-            max_index = std::max(max_index,
-                    *std::max_element(idxs.begin(), idxs.end()));
+            os << '#' << c << '\n';
         }
     }
-    const std::size_t idx_width = std::to_string(max_index).size();
+    os << "[[forcefields.local]]\n";
 
-    std::vector<std::string> keys;
-    for(const auto& kv : params.front().cast<toml::value_t::Table>())
+    // ========================================================================
+    // output miscellaneous stuff in the predefined order
+
+    assert(toml::find(ff, "interaction").comment().empty());
+    os << "interaction = " << toml::find(ff, "interaction") << '\n';
+    if(ff.as_table().count("potential") == 1)
+    {
+        assert(toml::find(ff, "potential").comment().empty());
+        os << "potential   = " << toml::find(ff, "potential") << '\n';
+    }
+    if(ff.as_table().count("topology") == 1)
+    {
+        assert(toml::find(ff, "topology").comment().empty());
+        os << "topology    = " << toml::find(ff, "topology") << '\n';
+    }
+
+    if(ff.as_table().count("env") == 1)
+    {
+        for(const auto& kv : toml::find(ff, "env").as_table())
+        {
+            assert(kv.second.comment().empty());
+            // TODO later (toml11 v3.0.0), use toml::format_key(kv.first) here
+            os << "env." << kv.first << " = " << kv.second << '\n';
+        }
+    }
+
+    // output other potential-specific paramters, if exists
+    for(const auto& kv : ff.as_table())
     {
         const auto& key = kv.first;
-        if(key != "indices")
+        if(key == "potential" || key == "interaction" || key == "topology" ||
+           key == "env"       || key == "parameters")
         {
-            keys.push_back(key);
+            // these keys has special meaning, so output in a different way
+            continue;
         }
+        assert(kv.second.comment().empty());
+        os << key << " = " << kv.second;
     }
-    // and sort keys in alphabetical order to make the order uniform
+
+    // ========================================================================
+    // output `parameters` field in an array-of-inline-tables way.
+
+    // ------------------------------------------------------------------------
+    // find the maximum index and calculate the max width to write them
+
+    std::size_t max_index = 0;
+    for(const auto& p : toml::find(ff, "parameters").as_array())
+    {
+        if(p.as_table().count("indices") == 0)
+        {
+            write_error(os, "`parameters` does not has `indices` field");
+        }
+        const auto idxs = toml::find<std::vector<std::size_t>>(p, "indices");
+        max_index = std::max(max_index, *std::max_element(idxs.begin(), idxs.end()));
+    }
+    const auto idx_width = std::to_string(max_index).size();
+
+    // ------------------------------------------------------------------------
+    // collect and sort keys in a table it to output them in a fixed order
+
+    std::vector<std::string> keys;
+    for(const auto& kv : toml::find(ff, "parameters").as_array().front())
+    {
+        if(kv.first != "indices") {keys.push_back(kv.first);}
+    }
     std::sort(keys.begin(), keys.end());
-    /* ================================ end! ================================ */
 
     os << "parameters = [\n";
-    for(const auto& p : params)
+    for(const auto& p : toml::find(ff, "parameters").as_array())
     {
-        const auto& ptable = p.cast<toml::value_t::Table>();
-        os << '{';
-        // write indices first
-        if(ptable.count("indices"))
+        // write comment if exists
+        if(!p.comment().empty())
         {
-            const auto idxs = toml::get<std::vector<std::size_t>>(
-                mjolnir::toml_value_at(ptable, "indices",
-                    "jarngreipr: generated local forcefield"));
+            // TODO later (toml11-v3.0.0), this can be refactored into
+            //      `os << local.comment();`
+            for(const auto& c : p.comment())
+            {
+                os << '#' << c << '\n';
+            }
+        }
 
-            os << "indices = [";
+        // write indices first
+        os << "{indices = [";
+        {
+            const auto idxs = toml::find<std::vector<std::size_t>>(p, "indices");
             for(auto iter = idxs.begin(); iter != idxs.end(); ++iter)
             {
                 if(iter != idxs.begin()) {os << ',';}
-                const auto& idx = *iter;
-                os << std::setw(idx_width) << idx;
+                os << std::setw(idx_width) << *iter;
             }
-            os << "], ";
         }
+        os << ']';
 
-        for(auto iter = keys.begin(); iter != keys.end(); ++iter)
+        // write other keys in the fixed order
+        for(const auto& key : keys)
         {
-            if(iter != keys.begin()) {os << ", ";}
-            const auto& key = *iter;
-            const auto& val = mjolnir::toml_value_at(ptable, key,
-                    "jarngreipr: generated local forcefield");
-            os << key << " = ";
-            write_toml_value(os, val);
+            assert(toml::find(p, key).comment().empty());
+            os << ", " << key << " = " << toml::find(p, key);
         }
         os << "},\n";
     }
     os << "]\n";
-
-    return ;
+    return os;
 }
 
-template<typename charT, typename traits>
-void write_global_forcefield(
-        std::basic_ostream<charT, traits>& os, const toml::Table& ff)
+template<typename charT, typename traits, typename Comment,
+         template<typename...> class Map, template<typename...> class Array>
+std::basic_ostream<charT, traits>&
+write_global_forcefield(std::basic_ostream<charT, traits>& os,
+                        const toml::basic_value<Comment, Map, Array>& ff)
 {
+    if(!ff.comment().empty())
+    {
+        for(const auto& c : ff.comment())
+        {
+            os << '#' << c << '\n';
+        }
+    }
     os << "[[forcefields.global]]\n";
 
+    // ========================================================================
+    // output interaction, potential, ignore, spatial_partition, and env first
+
+    assert(toml::find(ff, "interaction").comment().empty());
+    os << "interaction = " << toml::find(ff, "interaction") << '\n';
+
+    assert(toml::find(ff, "potential").comment().empty());
+    os << "potential   = " << toml::find(ff, "potential") << '\n';
+
+    for(const auto& kv : toml::find(ff, "ignore").as_table())
     {
-        std::vector<std::string> keys;
-        for(const auto& kv : ff)
+        assert(kv.second.comment().empty());
+        // TODO toml::format_key(kv.first)
+        os << "ignore." << kv.first << " = " << kv.second << '\n';
+    }
+
+    for(const auto& kv : toml::find(ff, "spatial_partition").as_table())
+    {
+        assert(kv.second.comment().empty());
+        // TODO toml::format_key(kv.first)
+        os << "spatial_partition." << kv.first << " = " << kv.second << '\n';
+    }
+
+    if(ff.as_table().count("env") == 1)
+    {
+        for(const auto& kv : toml::find(ff, "env").as_table())
         {
-            if(kv.first != "parameters") {keys.push_back(kv.first);}
+            assert(kv.second.comment().empty());
+            // TODO toml::format_key(kv.first)
+            os << "env." << kv.first << " = " << kv.second << '\n';
         }
-        std::sort(keys.begin(), keys.end());
+    }
+
+    // ------------------------------------------------------------------------
+    // output other potential-specific paramters, if exists
+    for(const auto& kv : ff.as_table())
+    {
+        const auto& key = kv.first;
+        if(key == "potential" || key == "interaction" || key == "ignore" ||
+           key == "spatial_partition" || key == "env" || key == "parameters")
+        {
+            // these keys has special meaning, so output in a different way
+            continue;
+        }
+        assert(kv.second.comment().empty());
+        // TODO toml::format_key(kv.first)
+        os << key << " = " << kv.second;
+    }
+
+    // ========================================================================
+    // output parameters = [{...}, ...]
+
+    // ------------------------------------------------------------------------
+    // find the maximum index and calculate the max width to write them
+
+    std::size_t max_index = 0;
+    for(const auto& p : toml::find(ff, "parameters").as_array())
+    {
+        if(p.as_table().count("index") == 0)
+        {
+            write_error(os, "`parameters` does not has `index` field");
+        }
+        max_index = std::max(max_index, toml::find<std::size_t>(p, "index"));
+    }
+    const auto idx_width = std::to_string(max_index).size();
+
+    // ------------------------------------------------------------------------
+    // collect and sort keys in a table it to output them in a fixed order
+
+    std::vector<std::string> keys;
+    for(const auto& kv : toml::find(ff, "parameters").as_array().front())
+    {
+        if(kv.first != "indices") {keys.push_back(kv.first);}
+    }
+    std::sort(keys.begin(), keys.end());
+
+    // ------------------------------------------------------------------------
+    // output parameters
+
+    os << "parameters = [\n";
+    for(const auto& p : toml::find(ff, "parameters").as_array())
+    {
+        os << "{index = "
+           << std::setw(idx_width) << toml::find<std::size_t>(p, "index");
 
         for(const auto& key : keys)
         {
-            os << key << " = ";
-            write_toml_value(os, mjolnir::toml_value_at(ff, key,
-                    "jarngreipr: generated global forcefield"));
-            os << '\n';
+            assert(toml::find(p, key).comments().empty());
+            os << ", " << key << " = " << toml::find(p, key);
         }
+        os << "},\n";
     }
-
-    {
-        const auto& params = mjolnir::toml_value_at(
-            ff, "parameters", "write_global_forcefield"
-            ).cast<toml::value_t::Array>();
-        if(params.empty()) {return;}
-
-        /* ========== prepair some values for formating parameters ========== */
-        std::size_t max_index = 0;
-        for(const auto& p : params)
-        {
-            const auto& ptable = p.cast<toml::value_t::Table>();
-            if(ptable.count("index"))
-            {
-                const auto idx = toml::get<std::size_t>(
-                    mjolnir::toml_value_at(ptable, "index",
-                        "jarngreipr: generated local forcefield"));
-                max_index = std::max(max_index, idx);
-            }
-        }
-        const std::size_t idx_width = std::to_string(max_index).size();
-
-        std::vector<std::string> keys;
-        for(const auto& kv : params.front().cast<toml::value_t::Table>())
-        {
-            const auto& key = kv.first;
-            if(key != "index")
-            {
-                keys.push_back(key);
-            }
-        }
-        // and sort keys in alphabetical order to make the order uniform
-        std::sort(keys.begin(), keys.end());
-        /* ============================== end! ============================== */
-
-        os << "parameters = [\n";
-        for(const auto& p : params)
-        {
-            const auto& ptable = p.cast<toml::value_t::Table>();
-            os << '{';
-            // write index first
-            if(ptable.count("index"))
-            {
-                const auto idx = toml::get<std::size_t>(mjolnir::toml_value_at(
-                    ptable, "index", "jarngreipr: generated local forcefield"));
-                os << "index = "<< std::setw(idx_width) << idx << ", ";
-            }
-
-            for(auto iter = keys.begin(); iter != keys.end(); ++iter)
-            {
-                if(iter != keys.begin()) {os << ", ";}
-                const auto& key = *iter;
-                const auto& val = mjolnir::toml_value_at(ptable, key,
-                        "jarngreipr: generated local forcefield");
-                os << key << " = ";
-                write_toml_value(os, val);
-            }
-            os << "},\n";
-        }
-        os << "]\n";
-    }
-    return ;
+    os << "]\n";
+    return os;
 }
 
-template<typename charT, typename traits>
-void write_forcefield(
-        std::basic_ostream<charT, traits>& os, const toml::Table& ff)
+template<typename charT, typename traits, typename Comment,
+         template<typename...> class Map, template<typename...> class Array>
+std::basic_ostream<charT, traits>&
+write_forcefield(std::basic_ostream<charT, traits>& os,
+                 const toml::basic_value<Comment, Map, Array>& ff)
 {
     os << "[[forcefields]]\n";
-
-    if(ff.count("local") == 1)
+    if(ff.as_table().count("local") == 1)
     {
-        for(const auto& local : ff.at("local").cast<toml::value_t::Array>())
+        for(const auto& local : ff.as_table().at("local").as_array())
         {
-            write_local_forcefield(os, local.cast<toml::value_t::Table>());
+            write_local_forcefield(os, local);
         }
     }
-    if(ff.count("global") == 1)
+    if(ff.as_table().count("global") == 1)
     {
-        for(const auto& global : ff.at("global").cast<toml::value_t::Array>())
+        for(const auto& global : ff.as_table().at("global").as_array())
         {
-            write_global_forcefield(os, global.cast<toml::value_t::Table>());
+            write_global_forcefield(os, global);
         }
     }
-    return;
+    return os;
 }
 
 } // jarngreipr
--- jarngreipr/io/write_system.hpp
+++ jarngreipr/io/write_system.hpp
@@ -1,60 +1,74 @@
 #ifndef JARNGREIPR_WRITE_SYSTEM_HPP
 #define JARNGREIPR_WRITE_SYSTEM_HPP
 #include <extlib/toml/toml.hpp>
-#include <mjolnir/util/get_toml_value.hpp>
 #include <iomanip>
 
 namespace jarngreipr
 {
 
-template<typename charT, typename traits>
-void write_system(std::basic_ostream<charT, traits>& os, const toml::Table& sys)
+template<typename charT, typename traits, typename Comment,
+         template<typename...> class Map, template<typename...> class Array>
+std::basic_ostream<charT, traits>&
+write_system(std::basic_ostream<charT, traits>& os,
+             const toml::basic_value<Comment, Map, Array>& sys)
 {
+    if(!sys.comment().empty())
+    {
+        // TODO later (toml11-v3.0.0), this can be refactored into
+        //      `os << sys.comment();`
+        for(const auto& c : sys.comment())
+        {
+            os << '#' << c << '\n';
+        }
+    }
     os << "[[systems]]\n";
-    os << "attributes = ";
-    write_toml_value(os,
-            mjolnir::toml_value_at(sys, "attributes", "write_system::sys"));
-    os << '\n';
 
-    os << "boundary = ";
-    write_toml_value(os,
-            mjolnir::toml_value_at(sys, "boundary", "write_system::sys"));
-    os << '\n';
+    if(toml::find(sys, "boundary").as_table().empty())
+    {
+        os << "boundary = {} # unlimited\n";
+    }
+    else
+    {
+        for(const auto& kv : toml::find(sys, "boundary").as_table())
+        {
+            assert(kv.second.comment().empty());
+            // TODO later (toml11 v3.0.0), use toml::format_key(kv.first) here
+            os << "boundary." << kv.first << " = " << kv.second << '\n';
+        }
+    }
 
-    const auto& ps = mjolnir::toml_value_at(
-        sys, "particles", "write_system::sys").cast<toml::value_t::Array>();
+    if(sys.as_table().count("attributes") == 1)
+    {
+        for(const auto& kv : toml::find(sys, "attributes").as_table())
+        {
+            assert(kv.second.comment().empty());
+            // TODO later (toml11 v3.0.0), use toml::format_key(kv.first) here
+            os << "attributes." << kv.first << " = " << kv.second << '\n';
+        }
+    }
 
     os << "particles = [\n";
-    for(const auto& item : ps)
+    for(const auto& particle : toml::find(sys, "particles").as_table())
     {
-        const auto& particle = item.cast<toml::value_t::Table>();
-        const auto m = toml::get<double>(mjolnir::toml_value_at(
-            particle, "mass", "write_system: particles"));
-        const auto p = toml::get<std::array<double, 3>>(mjolnir::toml_value_at(
-            particle, "position", "write_system: particles"));
-        const auto v = toml::get<std::array<double, 3>>(mjolnir::toml_value_at(
-            particle, "velocity", "write_system: particles"));
+        const auto m = toml::find<double>(particle, "mass");
+        const auto p = toml::find<std::array<double, 3>>(particle, "position");
+        const auto v = toml::find<std::array<double, 3>>(particle, "velocity");
+        const auto n = toml::find<std::string>(particle, "name");
+        const auto g = toml::find<std::string>(particle, "group");
 
-        os << "{mass=" << std::setw(7) << std::setprecision(2)
-           << std::fixed << std::right << m;
-        os << ",position=["
-           << std::setw(9) << std::setprecision(4) << std::fixed << std::right
-           << p[0] << ','
-           << std::setw(9) << std::setprecision(4) << std::fixed << std::right
-           << p[1] << ','
-           << std::setw(9) << std::setprecision(4) << std::fixed << std::right
-           << p[2];
-        os << "],velocity=["
-           << std::setw(9) << std::setprecision(4) << std::fixed << std::right
-           << v[0] << ','
-           << std::setw(9) << std::setprecision(4) << std::fixed << std::right
-           << v[1] << ','
-           << std::setw(9) << std::setprecision(4) << std::fixed << std::right
-           << v[2];
-        os << "]},\n";
+        os << "{m=" << std::setw(7) << std::fixed << std::right << m;
+        os << ",pos=["
+           << std::setw(9) << std::fixed << std::right << p[0] << ','
+           << std::setw(9) << std::fixed << std::right << p[1] << ','
+           << std::setw(9) << std::fixed << std::right << p[2];
+        os << "],vel=["
+           << std::setw(9) << std::fixed << std::right << v[0] << ','
+           << std::setw(9) << std::fixed << std::right << v[1] << ','
+           << std::setw(9) << std::fixed << std::right << v[2];
+        os << "], name = " << n << ", group = " << g << "},\n";
     }
     os << "]\n";
-    return ;
+    return os;
 }
 
 } // jarngreipr
