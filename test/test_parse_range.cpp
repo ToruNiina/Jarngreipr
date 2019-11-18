f72e70ee7bdf4bac0c4154c45eef8c78eaa1d4c0
ToruNiina <niina.toru.68u@gmail.com>
2019-11-18 13:28:39 +0900

    :white_check_mark: add test for chain range

--- test/test_parse_range.cpp
+++ test/test_parse_range.cpp
@@ -11,83 +11,83 @@ BOOST_AUTO_TEST_CASE(test_parse_range_simple)
 {
     {
         const auto range = jarngreipr::parse_range("[1,10]"_str);
-        BOOST_TEST(range.size(), 10);
+        BOOST_TEST(range.size() == 10u);
         for(std::int64_t i=0; i<10; ++i)
         {
-            BOOST_TEST(range.at(i), i+1);
+            BOOST_TEST(range.at(i) == i+1);
         }
     }
     {
         const auto range = jarngreipr::parse_range("[1,  10]"_str);
-        BOOST_TEST(range.size(), 10);
+        BOOST_TEST(range.size() == 10u);
         for(std::int64_t i=0; i<10; ++i)
         {
-            BOOST_TEST(range.at(i), i+1);
+            BOOST_TEST(range.at(i) == i+1);
         }
     }
     {
         const auto range = jarngreipr::parse_range("[1, 10)"_str);
-        BOOST_TEST(range.size(), 9);
+        BOOST_TEST(range.size() == 9u);
         for(std::int64_t i=0; i<9; ++i)
         {
-            BOOST_TEST(range.at(i), i+1);
+            BOOST_TEST(range.at(i) == i+1);
         }
     }
     {
         const auto range = jarngreipr::parse_range("(1, 10)"_str);
-        BOOST_TEST(range.size(), 8);
+        BOOST_TEST(range.size() == 8u);
         for(std::int64_t i=0; i<8; ++i)
         {
-            BOOST_TEST(range.at(i), i+2);
+            BOOST_TEST(range.at(i) == i+2);
         }
     }
     {
         const auto range = jarngreipr::parse_range("(1, 10]"_str);
-        BOOST_TEST(range.size(), 9);
+        BOOST_TEST(range.size() == 9u);
         for(std::int64_t i=0; i<9; ++i)
         {
-            BOOST_TEST(range.at(i), i+2);
+            BOOST_TEST(range.at(i) == i+2);
         }
     }
 
     {
         const auto range = jarngreipr::parse_range("[ 1,10 ]"_str);
-        BOOST_TEST(range.size(), 10);
+        BOOST_TEST(range.size() == 10u);
         for(std::int64_t i=0; i<10; ++i)
         {
-            BOOST_TEST(range.at(i), i+1);
+            BOOST_TEST(range.at(i) == i+1);
         }
     }
     {
         const auto range = jarngreipr::parse_range("[  1,  10 ]"_str);
-        BOOST_TEST(range.size(), 10);
+        BOOST_TEST(range.size() == 10u);
         for(std::int64_t i=0; i<10; ++i)
         {
-            BOOST_TEST(range.at(i), i+1);
+            BOOST_TEST(range.at(i) == i+1);
         }
     }
     {
         const auto range = jarngreipr::parse_range("[ 1, 10 )"_str);
-        BOOST_TEST(range.size(), 9);
+        BOOST_TEST(range.size() == 9u);
         for(std::int64_t i=0; i<9; ++i)
         {
-            BOOST_TEST(range.at(i), i+1);
+            BOOST_TEST(range.at(i) == i+1);
         }
     }
     {
         const auto range = jarngreipr::parse_range("( 1, 10 )"_str);
-        BOOST_TEST(range.size(), 8);
+        BOOST_TEST(range.size() == 8u);
         for(std::int64_t i=0; i<8; ++i)
         {
-            BOOST_TEST(range.at(i), i+2);
+            BOOST_TEST(range.at(i) == i+2);
         }
     }
     {
         const auto range = jarngreipr::parse_range("( 1, 10 ]"_str);
-        BOOST_TEST(range.size(), 9);
+        BOOST_TEST(range.size() == 9u);
         for(std::int64_t i=0; i<9; ++i)
         {
-            BOOST_TEST(range.at(i), i+2);
+            BOOST_TEST(range.at(i) == i+2);
         }
     }
 }
@@ -96,132 +96,150 @@ BOOST_AUTO_TEST_CASE(test_parse_range_signs)
 {
     {
         const auto range = jarngreipr::parse_range("[+1, +10]"_str);
-        BOOST_TEST(range.size(), 10);
+        BOOST_TEST(range.size() == 10u);
         for(std::int64_t i=0; i<10; ++i)
         {
-            BOOST_TEST(range.at(i), i+1);
+            BOOST_TEST(range.at(i) == i+1);
         }
     }
     {
         const auto range = jarngreipr::parse_range("[+1, +10)"_str);
-        BOOST_TEST(range.size(), 9);
+        BOOST_TEST(range.size() == 9u);
         for(std::int64_t i=0; i<9; ++i)
         {
-            BOOST_TEST(range.at(i), i+1);
+            BOOST_TEST(range.at(i) == i+1);
         }
     }
     {
         const auto range = jarngreipr::parse_range("(+1, +10)"_str);
-        BOOST_TEST(range.size(), 8);
+        BOOST_TEST(range.size() == 8u);
         for(std::int64_t i=0; i<8; ++i)
         {
-            BOOST_TEST(range.at(i), i+2);
+            BOOST_TEST(range.at(i) == i+2);
         }
     }
     {
         const auto range = jarngreipr::parse_range("(+1, +10]"_str);
-        BOOST_TEST(range.size(), 9);
+        BOOST_TEST(range.size() == 9u);
         for(std::int64_t i=0; i<9; ++i)
         {
-            BOOST_TEST(range.at(i), i+2);
+            BOOST_TEST(range.at(i) == i+2);
         }
     }
     {
         const auto range = jarngreipr::parse_range("[-10, -1]"_str);
-        BOOST_TEST(range.size(), 10);
+        BOOST_TEST(range.size() == 10u);
         for(std::int64_t i=0; i<10; ++i)
         {
-            BOOST_TEST(range.at(i), i-10); // from -10 to -1
+            BOOST_TEST(range.at(i) == i-10); // from -10 to -1
         }
     }
     {
         const auto range = jarngreipr::parse_range("[-10, -1)"_str);
-        BOOST_TEST(range.size(), 9);
+        BOOST_TEST(range.size() == 9u);
         for(std::int64_t i=0; i<9; ++i)
         {
-            BOOST_TEST(range.at(i), i-10);
+            BOOST_TEST(range.at(i) == i-10);
         }
     }
     {
         const auto range = jarngreipr::parse_range("(-10, -1)"_str);
-        BOOST_TEST(range.size(), 8);
+        BOOST_TEST(range.size() == 8u);
         for(std::int64_t i=0; i<8; ++i)
         {
-            BOOST_TEST(range.at(i), i-9);
+            BOOST_TEST(range.at(i) == i-9);
         }
     }
     {
         const auto range = jarngreipr::parse_range("(-10, -1]"_str);
-        BOOST_TEST(range.size(), 9);
+        BOOST_TEST(range.size() == 9u);
         for(std::int64_t i=0; i<9; ++i)
         {
-            BOOST_TEST(range.at(i), i-9);
+            BOOST_TEST(range.at(i) == i-9);
         }
     }
 
 
     {
         const auto range = jarngreipr::parse_range("[ +1, +10 ]"_str);
-        BOOST_TEST(range.size(), 10);
+        BOOST_TEST(range.size() == 10u);
         for(std::int64_t i=0; i<10; ++i)
         {
-            BOOST_TEST(range.at(i), i+1);
+            BOOST_TEST(range.at(i) == i+1);
         }
     }
     {
         const auto range = jarngreipr::parse_range("[ +1, +10 )"_str);
-        BOOST_TEST(range.size(), 9);
+        BOOST_TEST(range.size() == 9u);
         for(std::int64_t i=0; i<9; ++i)
         {
-            BOOST_TEST(range.at(i), i+1);
+            BOOST_TEST(range.at(i) == i+1);
         }
     }
     {
         const auto range = jarngreipr::parse_range("( +1, +10 )"_str);
-        BOOST_TEST(range.size(), 8);
+        BOOST_TEST(range.size() == 8u);
         for(std::int64_t i=0; i<8; ++i)
         {
-            BOOST_TEST(range.at(i), i+2);
+            BOOST_TEST(range.at(i) == i+2);
         }
     }
     {
         const auto range = jarngreipr::parse_range("( +1, +10 ]"_str);
-        BOOST_TEST(range.size(), 9);
+        BOOST_TEST(range.size() == 9u);
         for(std::int64_t i=0; i<9; ++i)
         {
-            BOOST_TEST(range.at(i), i+2);
+            BOOST_TEST(range.at(i) == i+2);
         }
     }
     {
         const auto range = jarngreipr::parse_range("[ -10, -1 ]"_str);
-        BOOST_TEST(range.size(), 10);
+        BOOST_TEST(range.size() == 10u);
         for(std::int64_t i=0; i<10; ++i)
         {
-            BOOST_TEST(range.at(i), i-10); // from -10 to -1
+            BOOST_TEST(range.at(i) == i-10); // from -10 to -1
         }
     }
     {
         const auto range = jarngreipr::parse_range("[ -10, -1 )"_str);
-        BOOST_TEST(range.size(), 9);
+        BOOST_TEST(range.size() == 9u);
         for(std::int64_t i=0; i<9; ++i)
         {
-            BOOST_TEST(range.at(i), i-10);
+            BOOST_TEST(range.at(i) == i-10);
         }
     }
     {
         const auto range = jarngreipr::parse_range("( -10, -1 )"_str);
-        BOOST_TEST(range.size(), 8);
+        BOOST_TEST(range.size() == 8u);
         for(std::int64_t i=0; i<8; ++i)
         {
-            BOOST_TEST(range.at(i), i-9);
+            BOOST_TEST(range.at(i) == i-9);
         }
     }
     {
         const auto range = jarngreipr::parse_range("( -10, -1 ]"_str);
-        BOOST_TEST(range.size(), 9);
+        BOOST_TEST(range.size() == 9u);
         for(std::int64_t i=0; i<9; ++i)
         {
-            BOOST_TEST(range.at(i), i-9);
+            BOOST_TEST(range.at(i) == i-9);
         }
+    }
+}
+
+BOOST_AUTO_TEST_CASE(test_parse_range_chains)
+{
+    {
+        const auto range = jarngreipr::parse_range<std::string>("A:B"_str);
+        BOOST_TEST(range.size() == 2u);
+        BOOST_TEST(range.at(0) == "A");
+        BOOST_TEST(range.at(1) == "B");
+    }
+    {
+        const auto range = jarngreipr::parse_range<std::string>("C:F"_str);
+        BOOST_TEST(range.size() == 4u);
+        BOOST_TEST(range.at(0) == "C");
+        BOOST_TEST(range.at(1) == "D");
+        BOOST_TEST(range.at(2) == "E");
+        BOOST_TEST(range.at(3) == "F");
     }
 }
